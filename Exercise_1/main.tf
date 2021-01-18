873fcc9029cc05999ce4ec1c64a37033452c2b43
Alexandru Dima <alexandru.dima@flywire.com>
2021-01-18 13:19:39 +0200

    Update files after review

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -11,17 +11,17 @@ resource "aws_instance" "UdacityT2" {
   instance_type = "t2.micro"
 
   tags = {
-    Purpose = "PlayNTest"
+    Type = "Udacity T2"
   }
 }
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-# resource "aws_instance" "UdacityM4" {
-#   count         = "2"
-#   ami           = "ami-0323c3dd2da7fb37d"
-#   instance_type = "m4.large"
+resource "aws_instance" "UdacityM4" {
+  count         = "2"
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "m4.large"
 
-#   tags = {
-#     Purpose = "PlayNTest"
-#   }
-# }
+  tags = {
+    Type = "Udacity M4"
+  }
+}
